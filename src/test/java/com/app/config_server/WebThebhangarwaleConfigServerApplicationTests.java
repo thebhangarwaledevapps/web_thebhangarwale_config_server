cd614442c1b4e0ad464c620e265e7ba0721765d7
thebhangarwale devapps <thebhangarwaledevapps@gmail.com>
2023-01-17 20:15:35 +0530

    Config Server Setup

---
+++ src/test/java/com/app/config_server/WebThebhangarwaleConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebThebhangarwaleConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
